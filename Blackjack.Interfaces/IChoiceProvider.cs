a2eba34c8bffe9b527ff9ae8e1563f6eeea983c6
Travis Alcantara <v-tralca@microsoft.com>
2018-01-30 13:01:20 -0800

    Setup IGameManager and IChoiceProvider interfaces

---
+++ Blackjack.Interfaces/IChoiceProvider.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blackjack.Interfaces
+{
+    public enum Choice
+    {
+        Hit,
+        Stay
+    }
+    interface IChoiceProvider
+    {
+        void DirectHitChoice(IPlayer gambler, IPlayer dealer, Choice choice);
+    }
+}
---
+++ Blackjack.Interfaces/IGameManager.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blackjack.Interfaces
+{
+    interface IGameManager
+    {
+        void DirectGamblerChoice(IInputProvider inputProvider, IChoiceProvider choiceProvider, IPlayer gambler, IPlayer dealer);
+    }
+}
