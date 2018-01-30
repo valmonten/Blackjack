37ddc418c82bbb028731f0475973d591cd7e241f
Jennifer Feng <jjfeng520@gmail.com>
2018-01-29 23:04:50 -0800

    Add a test to test Card constructor

--- Blackjack.Tests/CardTests.cs
+++ Blackjack.Tests/CardTests.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Blackjack.Interfaces;
 
 namespace Blackjack.Tests
 {
@@ -7,8 +8,20 @@ namespace Blackjack.Tests
     public class CardTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void TestCardConstructor()
         {
+            // Arrange
+            Suit testSuit = 0;
+            Face testFace = 0;
+            int testValue = 1;
+
+            // Act
+            Card actual = new Card(testSuit, testFace, testValue);
+
+            // Assert
+            Assert.AreEqual("Heart", actual.Suit);
+            Assert.AreEqual("Ace", actual.Face);
+            Assert.AreEqual(1, actual.Value);
         }
     }
 }
