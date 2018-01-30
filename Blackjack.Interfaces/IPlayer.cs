bff63429da3967d61d153c863fc942d6244ebaf9
v-navalm <v-navalm@MININT-ARGH3O7>
2018-01-29 17:40:31 -0800

    Added properties and methods for IPlayer.cs

--- Blackjack.Interfaces/IPlayer.cs
+++ Blackjack.Interfaces/IPlayer.cs
@@ -6,7 +6,20 @@ using System.Threading.Tasks;
 
 namespace Blackjack.Interfaces
 {
-    public class IPlayer
+    // Interface for players
+    public interface IPlayer
     {
+        // The player's name
+        string Name { get; set; }
+
+        // The cards the player has
+        ICard[] Hand { get; set; }
+
+        // Hit me asks dealer for another card that gets added to Hand
+        ICard HitMe();
+
+        // Player takes no more cards
+        void Stay();
+
     }
 }
