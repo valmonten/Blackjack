4342b7c40b4165850ab14fd5046b0b860426bfcb
Mary Martin <v-martim@microsoft.com>
2018-01-30 15:52:09 -0800

    Commit to avoid data loss

--- Blackjack/ConsoleTableRenderer.cs
+++ Blackjack/ConsoleTableRenderer.cs
@@ -9,11 +9,25 @@ namespace Blackjack
 {
     public class ConsoleTableRenderer : ITableRenderer
     {
-        public void Render() : 
-        { }
         public void Render(ITable table)
         {
             Console.Write("Dealer's Cards: ");
+            //show dealer's visible cards
+            
+            //Go to a new line to display player's cards
+            Console.WriteLine();
+            
+            //show all player's cards
+            foreach(var player in table.Players)
+            {
+                Console.Write(player.ToString());
+            }
+        }
+        //helper function to show a player's hand.
+        public void ShowHand(IPlayer player)
+        {
+            //loop through a player's hand 
+            Console.Write("player's hand");
         }
     }
 }
