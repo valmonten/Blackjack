62ff9606a45539e7355ad0e4b3d3ad7ad4d26c58
Jennifer Feng <jjfeng520@gmail.com>
2018-01-29 22:24:13 -0800

    Setup tests for Card class

---
+++ Blackjack.Tests/CardTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Blackjack.Tests
+{
+    [TestClass]
+    public class CardTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
