e79d6ed1423edda77a32d599bd4296c8473a8849
Lizzie Gutierrez <v-elguti@microsoft.com>
2018-01-30 15:45:09 -0800

    Added template content into IDeck.

--- Blackjack.Interfaces/IDeck.cs
+++ Blackjack.Interfaces/IDeck.cs
@@ -8,5 +8,19 @@ namespace Blackjack.Interfaces
 {
     interface IDeck
     {
+        /// <summary>
+        /// Builds new deck of cards.
+        /// </summary>
+        void Build();
+
+        /// <summary>
+        /// Shuffles the deck.
+        /// </summary>
+        void Shuffle();
+
+        /// <summary>
+        /// Clear the deck completely.
+        /// </summary>
+        void Reset();
     }
 }
