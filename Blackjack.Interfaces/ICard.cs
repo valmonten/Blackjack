9e1aac4dd020c07ef23955f4047b0494d2ac4f8b
Jennifer Feng <jjfeng520@gmail.com>
2018-01-29 22:24:30 -0800

    Setup Card interface

---
+++ Blackjack.Interfaces/ICard.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blackjack.Interfaces
+{
+    public enum Suit
+    {
+        Heart,
+        Diamond,
+        Spade,
+        Club
+    }
+
+    public enum Face
+    {
+        Ace,
+        Two,
+        Three,
+        Four,
+        Five,
+        Six,
+        Seven,
+        Eight,
+        Nine,
+        Ten,
+        Jack,
+        Queen,
+        King
+    }
+
+    public interface ICard
+    {
+        Suit Suit { get; set; }
+        Face Face { get; set; }
+        int Value { get; set; }
+
+        /// <summary>
+        /// Function to create a card.
+        /// </summary>
+        void CreateCard(Suit suit, Face face, int value);
+    }
+}
