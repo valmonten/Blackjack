870f20256e34207f0879b0b81b4807a530b87498
Travis Alcantara <v-tralca@microsoft.com>
2018-01-30 16:58:39 -0800

    Added GameManagerTests

---
+++ Blackjack.Tests/GameManagerTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Blackjack.Tests
+{
+    [TestClass]
+    public class GameManagerTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
