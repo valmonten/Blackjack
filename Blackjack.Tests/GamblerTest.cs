6855053f8c699165be6c16ff75092b63fd2d7208
Luyao Wang <v-luwan@microsoft.com>
2018-01-30 18:09:00 -0800

    Added GamblerTest.

---
+++ Blackjack.Tests/GamblerTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Blackjack.Tests
+{
+    [TestClass]
+    public class GamblerTest
+    {
+        [TestMethod]
+        public void TestGamblerConstructor()
+        {
+            Gambler gambler = new Gambler("test");
+            Assert.AreEqual("test", gambler.Name);
+            Assert.AreEqual(0, gambler.Hand.Count);
+        }
+    }
+}
