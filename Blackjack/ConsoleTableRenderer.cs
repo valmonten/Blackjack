5dcd72d6cbee080d3e4c6e5341d178ca28c0cf97
Mary Martin <v-martim@microsoft.com>
2018-01-30 17:10:28 -0800

    Modularization in progress

--- Blackjack/ConsoleTableRenderer.cs
+++ Blackjack/ConsoleTableRenderer.cs
@@ -9,25 +9,57 @@ namespace Blackjack
 {
     public class ConsoleTableRenderer : ITableRenderer
     {
+        /// <summary>
+        /// Render the table to the user(s)
+        /// </summary>
+        /// <param name="table">Representation of the cards in play</param>
         public void Render(ITable table)
         {
-            Console.Write("Dealer's Cards: ");
-            //show dealer's visible cards
+            if(table == null)
+                throw new ArgumentNullException("Cannot render null");
+            if (table.AreAllHandsComplete)
+                ShowAllHands(table);
+            ShowPlayersHands(table);
             
-            //Go to a new line to display player's cards
-            Console.WriteLine();
-            
-            //show all player's cards
-            foreach(var player in table.Players)
-            {
-                Console.Write(player.ToString());
-            }
         }
-        //helper function to show a player's hand.
-        public void ShowHand(IPlayer player)
+
+        /// <summary>
+        /// Reveal all hands. Only to be called after all players have stayed or busted.
+        /// </summary>
+        /// <param name="table"></param>
+        public void ShowAllHands(ITable table)
         {
+            Console.Write("Dealer's Hand: ");
+            ShowHand(table.Dealer);
             //loop through a player's hand and print their cards
             Console.Write("player's hand");
+        }
+
+        /// <summary>
+        /// function to show only the players' hand(s) and limited dealer hand info
+        /// </summary>
+        /// <param name="table">ITable representation of the BlackJack table</param>
+        public void ShowPlayersHands(ITable table)
+        {
+
+        }
+
+        /// <summary>
+        /// reveal a single player's hands
+        /// </summary>
+        /// <param name="player">The IPlayer in the game</param>
+        public void ShowHand(IPlayer player)
+        {
+
+        }
+
+        /// <summary>
+        /// hide the last card dealt to the dealer
+        /// </summary>
+        /// <param name="dealer">The IDealer for the game</param>
+        public void HideHand(IDealer dealer)
+        {
+
         }
     }
 }
