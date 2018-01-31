6b7e70c8395ac3baeab3dce026da691bf5401c76
Luyao Wang <v-luwan@microsoft.com>
2018-01-30 18:03:49 -0800

    Added comments and constructors to Gambler.cs

--- Blackjack/Gambler.cs
+++ Blackjack/Gambler.cs
@@ -13,26 +13,42 @@ namespace Blackjack
         public string Name { get; set; }
         public List<ICard> Hand { get; set; }
 
+        public Gambler(string name)
+        {
+            Name = name;
+            Hand = new List<ICard>();
+        }
+
+
+        /// <summary>
+        /// For unit-testing.
+        /// </summary>
+        /// <param name="hand"></param>
+        /// <param name="name"></param>
+        public Gambler(List<ICard> hand, string name) : this(name)
+        {
+            Hand = hand;
+        }
+
+        /// <summary>
+        /// player can choose to Gamble, hence using the Money. Yet to implement.
+        /// </summary>
         public void Gamble()
         {
             throw new NotImplementedException();
         }
 
-        //public ICard HitMe(IDealer dealer)
-        //{
-        //    return dealer.Deal(this);
-        //}
-
+        /// <summary>
+        /// Player calls GetCard to get a card from the Dealer.
+        /// The Card is then added to Player's Hand.
+        /// </summary>
+        /// <param name="dealer"></param>
+        /// <returns>ICard</returns>
         public ICard GetCard(IDealer dealer)
         {
             ICard card = dealer.Deal();
             this.Hand.Add(card);
             return card;
         }
-
-        //public void Stay()
-        //{
-        //    throw new NotImplementedException();
-        //}
     }
 }
