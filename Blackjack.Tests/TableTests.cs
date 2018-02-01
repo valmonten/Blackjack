eae718c7e2ef44859ad951025b3dcb7d0358d648
Mary Martin <v-martim@microsoft.com>
2018-01-31 16:55:21 -0800

    Added additional test. Waiting to be able to confirm they work.

--- Blackjack.Tests/TableTests.cs
+++ Blackjack.Tests/TableTests.cs
@@ -2,6 +2,8 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Collections.Generic;
 using Blackjack.Interfaces;
+using Blackjack;
+using System.Linq;
 
 namespace Blackjack.Tests
 {
@@ -24,9 +26,21 @@ namespace Blackjack.Tests
         public void TestTableConstructorNoPlayers()
         {
             //arrange
-            var dealer = new Dealer();
+            var dealer = new Dealer("Bob");
             //act
             var table1 = new Table(dealer, null);
+        }
+        [TestMethod]
+        public void TestTableconstructorValid()
+        {
+            //arrange
+            var dealer = new Dealer("Bob");
+            List<Gambler> players = new List<Gambler> { new Gambler() };
+            //act
+            var table = new Table(dealer, players);
+            //assert
+            Assert.AreEqual(dealer.Name, table.Dealer.Name);
+            Assert.AreEqual(players.First().Name, table.Players.First().Name);
         }
     }
 }
